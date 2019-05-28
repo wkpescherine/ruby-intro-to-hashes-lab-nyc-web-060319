cba6b858779440365cd83768b926ab9711b6580e
William Pescherine <wkpescherine@gmail.com>
2019-05-28 04:36:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,8 @@
 def new_hash
-new_hash = {}
-return new_hash
+  actor = {
+    :name => "Dwayne the Rock Johnson"
+  }
+return actor
 end
 
 def actor
@@ -9,7 +11,7 @@ end
 
 def monopoly
 	monopoly = {}
-
+  monopoly[:railroads] = {}
 end
 
 def monopoly_with_second_tier
